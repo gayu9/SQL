a41cd4270de2d41a91f0c30e3a2d60ded1ccdc95
gayu9 <114666484+gayu9@users.noreply.github.com>
2022-10-28 15:07:24 +0530

    Update SQLQuery_GAYU.sql

--- SQLQuery_GAYU.sql
+++ SQLQuery_GAYU.sql
@@ -12,14 +12,16 @@ YEAR_OF_RELIEVING DATETIME
 INSERT INTO GAYATHRI_EMPLOYEES
 (ID,FIRST_NAME ,LAST_NAME ,SALARY ,DEPARTMENT_ID ,YEAR_OF_JOINING,YEAR_OF_RELIEVING )
 VALUES
-(11,'Raja','Lakshmi', 35000, 5,'09/20/2018','03/12/2022'),(12,'Maha','Lakshmi', 30000, 4,'07/09/2016','1/08/2020')
+(13,'','',40000 , 5,'5/29/2005','03/22/2022')
+
+,(12,'Maha','Lakshmi', 30000, 4,'07/09/2016','1/08/2020')
 
 (5,'Maha','Lakshmi', 30000, 3,'03/19/2018','02/10/2021'),(6,'Sathya','Kumar', 15000, 2,'04/19/2009','05/8/2012'),(7,'Surya','Prakash', 45000, 4,'06/19/2019','05/8/2012'),(8,'Aruna','Subramaniyan', 55000, 3,'07/20/2013','10/6/2015'),(9,'Sathya','priya', 10000, 5,'06/9/2020','11/21/2022'),(10,'Raja','Lakshmi', 18000, 2,'2/25/2013','01/28/2018')
 
 
 select * from GAYATHRI_EMPLOYEES
 
-update GAYATHRI_EMPLOYEES set FIRST_NAME='Saran' where ID=6
+update GAYATHRI_EMPLOYEES set LAST_NAME = null where  ID = 13
 
 
 
@@ -86,15 +88,79 @@ select FIRST_NAME,LAST_NAME from GAYATHRI_EMPLOYEES as g
   --20)
   select distinct year(YEAR_OF_JOINING) from GAYATHRI_EMPLOYEES  group by year(YEAR_OF_JOINING) having count(ID) >2
   --21)
-  select ID  from GAYATHRI_EMPLOYEES where SALARY >(select avg(SALARY) from GAYATHRI_EMPLOYEES)
+select ID  from GAYATHRI_EMPLOYEES where SALARY >(select avg(SALARY) from GAYATHRI_EMPLOYEES)
   --22)
   select datediff(day,YEAR_OF_JOINING,YEAR_OF_RELIEVING), FIRST_NAME from  GAYATHRI_EMPLOYEES 
 
   --23)
   select distinct year(YEAR_OF_JOINING) ,FIRST_NAME from GAYATHRI_EMPLOYEES  order by year(YEAR_OF_JOINING) 
 
-  --24)
+  --24)  
  select upper(FIRST_NAME)as first_name,lower(LAST_NAME)as last_name from GAYATHRI_EMPLOYEES
  --25)
-
+ select len(FIRST_NAME) as lengthofname,FIRST_NAME, LAST_NAME from GAYATHRI_EMPLOYEES where LAST_NAME like '__b%'
   --26)
+    select datediff(year,YEAR_OF_JOINING,YEAR_OF_RELIEVING) as Experience , FIRST_NAME from  GAYATHRI_EMPLOYEES 
+ --27)
+  select FIRST_NAME,LAST_NAME from GAYATHRI_EMPLOYEES where month(YEAR_OF_JOINING) = 5 
+  --28)
+  select ID,FIRST_NAME,LAST_NAME from GAYATHRI_EMPLOYEES where SALARY >(select avg(SALARY) from GAYATHRI_EMPLOYEES)
+  --29)
+  1.primary key means unique key table have only one primary key .
+  example:
+  CREATE TABLE GAYATHRI_EMPLOYEES
+(
+ID INT PRIMARY KEY,
+FIRST_NAME VARCHAR(40),
+LAST_NAME VARCHAR(40),
+SALARY DECIMAL(8,2),
+DEPARTMENT_ID INT,
+YEAR_OF_JOINING DATETIME,
+YEAR_OF_RELIEVING DATETIME
+)
+2. foreign key  is primary key of another table one table have one,more foreign key 
+example:
+
+CREATE TABLE GAYATHRI_DEPARTMENTS
+(
+DEPARTMENT_ID INT PRIMARY KEY,
+MANAGER_ID INT,
+DEPARTMENT_NAME VARCHAR(50)
+)
+in thi Depertment_id is  foreign keye of employee table.
+
+  --30)
+ update    GAYATHRI_EMPLOYEES set LAST_NAME='AAA' ,set FRST_NAME='AAA' where LAST_NAME ,FRST_NAME IS NULL
+ --31)
+ create view GAYATHRI_VIEW as select ID,FIRST_NAME,LAST_NAME,g.DEPARTMENT_ID,DEPARTMENT_NAME from  GAYATHRI_EMPLOYEES as g inner join  
+ GAYATHRI_DEPARTMENTS as d
+ on d.DEPARTMENT_ID=g.DEPARTMENT_ID ;
+
+--32)
+ create synonym gayu_sysn for  GAYATHRI_EMPLOYEES 
+
+ select * from gayu_sysn
+ 
+--33)
+create procedure Gayu_Procedure 
+as
+select * from GAYATHRI_EMPLOYEES where YEAR_OF_JOINING between '1/1/2010' and '12/31/2022'
+Go
+
+exec Gayu_Procedure
+--34) 
+create procedure Gayuu_Insert
+as
+insert into  GAYATHRI_DEPARTMENTS (DEPARTMENT_ID ,MANAGER_ID ,DEPARTMENT_NAME )
+values
+(11,6,'ADMIN')
+go
+
+exec Gayuu_Insert
+
+--35)
+select replace(convert(varchar,'2006-12-30',101),'-','') + replace(convert(varchar,' 00:38:54' ,108),':','');	
+
+
+
+
